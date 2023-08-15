192f44e4170eb5d48431538082196a20a136562b
comenda <pedro.incomenda@gmail.com>
2023-08-15 12:04:15 -0300

    update image.html

--- galeria/views.py
+++ galeria/views.py
@@ -6,6 +6,9 @@ def index(request):
     #return HttpResponse('<h1>Alura Space</h1>')
     return render(request, 'galeria/index.html')
 
+def imagem(request):
+    return render(request, 'geleria/imagem.html')
+
 
 
 # Create your views here.
