bb5a960f7107917f92d8c4c603c42e541a7c2360
comenda <pedro.incomenda@gmail.com>
2023-08-17 18:03:47 -0300

    configurando models

--- galeria/models.py
+++ galeria/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
 
+class Fotografia(models.Model):
+    # colocar o nome de cada uma das colunas da tabela 
+    nome = models.CharField(max_length=100, null=False, blank=False) # CharField; campo para char (caracteres)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
+
+#class Question(models.Model):
+#    question_text = models.CharField(max_length=200)
+#    pub_date = models.DateTimeField("date published")
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+#class Choice(models.Model):
+#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#    choice_text = models.CharField(max_length=200)
+#    votes = models.IntegerField(default=0)
 
 
 # Create your models here.
