a8bc74a4e99b9025b4776a422d424a3daf3cbf29
comenda <pedro.incomenda@gmail.com>
2023-09-14 10:47:07 -0300

    app usuarios

--- setup/urls.py
+++ setup/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     #path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
+    path('', include('usuarios.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ usuarios/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def login(request):
+    return render(request, "usuarios/login.html")
+
+def cadastro(request):
+    return render(request, "usuarios/cadastro.html")
+
+
+
+# Create your views here.
